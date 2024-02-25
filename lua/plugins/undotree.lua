f4be31aab78fc7d65c470ea5a4cb6b9e5dbefce9
Allison Durham <github@allisons.email>
2024-02-25 14:52:27 -0600

    undotree learned about

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,4 +1,7 @@
 return {
+	-- Way to view undo tree, which is important if swap/backup files are turned off.
+	-- TODO: Is this the approach I want to take for file saving? Do I need an autosave plugin to go alongside the undo file approach?
+	-- Source: https://github.com/jiaoshijie/undotree?tab=readme-ov-file
 	"jiaoshijie/undotree",
 	config = function()
 		local ut = require("undotree")
