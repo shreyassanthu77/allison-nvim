ed789c6383fdffd8ff73bfdafe56af4dc4776ce9
Allison Durham <github@allisons.email>
2024-02-25 14:18:50 -0600

    a couple more updates

--- lua/plugins/git-signs.lua
+++ lua/plugins/git-signs.lua
@@ -1,4 +1,6 @@
 return {
+	-- Seems pretty good out of the box. Supports integratino with folke/trouble.nvim which seems very interesting.
+	-- Source: https://github.com/lewis6991/gitsigns.nvim
 	"lewis6991/gitsigns.nvim",
 	config = function()
 		require("gitsigns").setup()
