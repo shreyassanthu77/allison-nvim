e7feb3adde9832fc15e76ef0626b966d6d11ed2e
Allison Durham <github@allisons.email>
2024-02-25 13:59:38 -0600

    dressings

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -1,4 +1,7 @@
 return {
+	-- I don't understand what this is doing and if/why I need it.
+	-- Source: https://github.com/stevearc/dressing.nvim
+	-- TODO: Consider removing and seeing the difference.
 	"stevearc/dressing.nvim",
 	opts = {}
 }
