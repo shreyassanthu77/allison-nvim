b6ca2143d8084339accc80b52e1eb072bbf43510
Allison Durham <a@AdjectiveAllison.com>
2024-08-15 10:03:22 -0500

    Removed code window cause I got errors and didn't want to troubleshoot

--- lua/plugins/minimap.lua
+++ lua/plugins/minimap.lua
@@ -1,13 +1,13 @@
 return {
-	-- Source: https://github.com/gorbit99/codewindow.nvim
-	"gorbit99/codewindow.nvim",
-	config = function()
-		local codewindow = require('codewindow')
-		codewindow.setup({
-			auto_enable = true,
-			minimap_width = 10,
-			width_multiplier = 4,
-		})
-		codewindow.apply_default_keybinds()
-	end,
+	-- 	-- Source: https://github.com/gorbit99/codewindow.nvim
+	-- 	"gorbit99/codewindow.nvim",
+	-- 	config = function()
+	-- 		local codewindow = require('codewindow')
+	-- 		codewindow.setup({
+	-- 			auto_enable = true,
+	-- 			minimap_width = 10,
+	-- 			width_multiplier = 4,
+	-- 		})
+	-- 		codewindow.apply_default_keybinds()
+	-- 	end,
 }
