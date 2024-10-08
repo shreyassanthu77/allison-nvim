43f445eff704bc3a34b9e21ff5d91d6eca7f84bd
Allison Durham <github-actions[bot]@users.noreply.github.com>
2024-10-08 05:02:17 -0700

    I like this approach best (easy edits no confirm but still confirm)

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -6,7 +6,7 @@ return {
 	config = function()
 		local oil = require("oil")
 		oil.setup({
-			skip_confirm_for_simple_edits = false,
+			skip_confirm_for_simple_edits = true,
 			view_options = {
 				show_hidden = true,
 			},
