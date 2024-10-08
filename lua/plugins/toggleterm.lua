d25472cbf5f4ead236ad4a4741e3169c4960817e
Allison Durham <github-actions[bot]@users.noreply.github.com>
2024-10-08 05:33:19 -0700

    add source link and comment on ugliness

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,4 +1,6 @@
 return {
+	-- Source: https://github.com/akinsho/toggleterm.nvim
+	-- Idk, I'm not sold with this yet. It seems sorta gross.
 	"akinsho/toggleterm.nvim",
 	version = "*",
 	opts = {},
